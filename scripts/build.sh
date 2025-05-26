5f44d85f3478fe81527d5748fa94d9e5a579141f
Dane Lipscombe <danelipscombe@gmail.com>
2025-05-26 15:53:03 +1000

    Add ref

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 source zephyr/zephyr-env.sh
-west build -s zmk/app -b seeeduino_xiao_ble -S zmk-usb-logging -- -DSHIELD=harite_v3_left -DZMK_CONFIG=$PWD/config
+west build -p -s zmk/app -b seeeduino_xiao_ble -S zmk-usb-logging -- -DSHIELD=harite_v3_left -DZMK_CONFIG=$PWD/config
