85725c2aa5a56721adaecaa0d6cdd836c035e42f
Dane Lipscombe <danelipscombe@gmail.com>
2025-07-26 21:51:13 +1000

    Add wired config

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 source zephyr/zephyr-env.sh
 SIDE=${1:-left}
-west build -p always -s zmk/app -b seeeduino_xiao_ble -S zmk-usb-logging -S studio-rpc-usb-uart -- -DSHIELD=harite_v3_${SIDE} -DZMK_CONFIG=$PWD/config -DCONFIG_ZMK_STUDIO=y
+# -S zmk-usb-logging
+west build -p always -s zmk/app -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DSHIELD=harite_v3_${SIDE} -DZMK_CONFIG=$PWD/config -DCONFIG_ZMK_STUDIO=y
