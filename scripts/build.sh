3cdd0447846687529d7438b72f21419ea4633565
Dane Lipscombe <danelipscombe@gmail.com>
2025-08-13 16:34:27 +1000

    Enable usb logging

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,4 @@
 set -euo pipefail
 source zephyr/zephyr-env.sh
 SIDE=${1:-left}
-# -S zmk-usb-logging
-west build -p always -s zmk/app -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DSHIELD=harite_v3_${SIDE} -DZMK_CONFIG=$PWD/config -DCONFIG_ZMK_STUDIO=y
+west build -p always -s zmk/app -S zmk-usb-logging -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DSHIELD=harite_v3_${SIDE} -DZMK_CONFIG=$PWD/config -DCONFIG_ZMK_STUDIO=y
