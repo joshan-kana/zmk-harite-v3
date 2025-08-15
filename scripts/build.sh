e6d222705a67b2678c58df19fd3d50b1e3cc3414
Dane Lipscombe <danelipscombe@gmail.com>
2025-08-15 11:09:08 +1000

    Add release

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 source zephyr/zephyr-env.sh
-SIDE=${1:-left}
-west build -p always -s zmk/app -S zmk-usb-logging -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DSHIELD=harite_v3_${SIDE} -DZMK_CONFIG=$PWD/config -DCONFIG_ZMK_STUDIO=y
+KEYMAP=${1}
+SIDE=${2}
+rm -f config/harite_v3.keymap
+ln -s "harite_v3_${KEYMAP}.keymap" config/harite_v3.keymap
+west build -p auto -s zmk/app -S zmk-usb-logging -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DSHIELD=harite_v3_${SIDE} -DZMK_CONFIG=$PWD/config -DCONFIG_ZMK_STUDIO=y
---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -rf release
+mkdir release
+keymaps=(default stained)
+sides=(left right)
+for keymap in "${keymaps[@]}"; do
+  for side in "${sides[@]}"; do
+    echo "Processing: $keymap $side"
+    scripts/build.sh ${keymap} ${side}
+    cp build/zephyr/zmk.uf2 release/${keymap}_${side}.uf2
+  done
+done
+
+scripts/reset.sh
+cp build/zephyr/zmk.uf2 release/reset.uf2
