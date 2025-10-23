58668d99e259f7bec1de278866eec0eb713db087
Joshan Kana <joshankana@gmail.com>
2025-10-24 00:28:43 +0800

    remove build directory per compilation to prevent wrong keyboard side being compiled

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,10 @@ if [ -z "${1:-}" ]; then
   echo "Error: build.sh <side(left/right)>"
 fi
 
+# if build directory exists, remove it (otherwise the previous side's firmware will not be overwritten)
+if [ -d build ]; then
+  rm -rf build
+fi
+
 SIDE=${1}
 west build -p auto -s zmk/app -S zmk-usb-logging -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- -DSHIELD=harite_v3_${SIDE} -DZMK_CONFIG=$PWD/config -DCONFIG_ZMK_STUDIO=y
